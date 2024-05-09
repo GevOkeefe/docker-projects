70a8506716db3ba936c10b607d799e92299519c5
Gevorg Arshakyan <gevorg@studioforty9.com>
2024-05-09 18:15:24 +0100

    Added shell script, docker compose file with configurations and ssh templates

---
+++ src/example/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Example template</title>
+</head>
+<body>
+    <div>This is an example!</div>
+    <div>Please place your projects under 'src' folder!</div>
+</body>
+</html>
